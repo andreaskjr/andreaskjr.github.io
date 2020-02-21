4f05be0e58e953a5fb244992142bd7fdac4aeca1
Andreas <andreaskjaer123@gmail.com>
2020-02-21 10:13:41 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 let img;
 let input;
 let border;
+let slider;
 // Indlæs image border effekt
     function preload() {
       border = loadImage('https://i.imgur.com/8FupOgP.png');
@@ -9,11 +10,15 @@ let border;
 // Setup med "gem billede" knap
     function setup() {
       createCanvas(400,400);
+      gennemsigtighed = 0;
       input = createFileInput(handleFile);
       input.position(400,0);
       button = createButton('Save Image');
       button.position(400,19);
       button.mousePressed(SAVE);
+      slider = createSlider(0, 255, 255, 0);
+      slider.position(410,150);
+      slider.style('width', '100px');
     }
 // Selve funktionen der laver billedet om til sort/hvid og smider border ovenpå.
     function draw() {
@@ -23,6 +28,13 @@ let border;
         c = map(mouseX,0,483,0,1);
         filter(GRAY, c);
         image(border, 0, 0, width, height);
+        border.loadPixels();
+        for (let i = 0; i < border.width; i++) {
+          for (let j = 0; j < border.height; j++) {
+            tint(255, slider.value());
+          }
+        }
+        border.updatePixels();
       }
     }
 // Fil kan uploades
@@ -42,3 +54,11 @@ function SAVE() {
   text('Gammeldags Billede Generator', 200, 390);
   saveCanvas('oldImage', 'jpg');
 }
+// function mouseClicked() {
+//  if (gennemsigtighed > 241) {
+//    return true;
+//  } else {
+//    gennemsigtighed = gennemsigtighed + 15;
+//    console.log(gennemsigtighed);
+//  }
+//}
